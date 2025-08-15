2a59764547c16a301d634f523658dde32e3bcc6e
Roman Vish <romanv7@byu.edu>
2025-08-15 16:05:59 -0600

    routing changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import CourseList from "@/views/CourseList.vue";
-import CourseDetail from "@/views/CourseDetail.vue";
 import AdminPanel from "@/views/AdminPanel.vue";
 import TracksList from "@/views/TracksList.vue";
 import TracksDetail from "@/views/TracksDetail.vue";
@@ -9,11 +7,7 @@ import SuperTracksList from "@/views/SuperTracksList.vue";
 const routes = [
   { path: "/tracks", name: "Home", component: SuperTracksList },
   { path: "/", redirect: "/tracks" },
-  // { path: "/courses/:id", name: "CourseDetail", component: CourseDetail, props: true },
-  // { path: "/tracks/:id", name: "TracksDetail", component: TracksDetail, props: true },
-  // { path: "/tracks/:name", name: "TracksList", component: TracksList, props: true },
   { path: "/admin", name: "AdminPanel", component: AdminPanel },
-  // { path: "/courses", name: "CourseList", component: CourseList },
   { path: "/tracks", name: "SupertracksList", component: SuperTracksList },
   { path: "/tracks/:supertrack/:track", name: "TrackDetail", component: TracksDetail, props: true },
   { path: "/tracks/:supertrack", name: "Supertrack", component: TracksList, props: true, }
